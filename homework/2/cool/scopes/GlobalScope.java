4890bf1b2f050ed12248e84c1bab4842422ed519
Matei Barbu <matei.barbu1905@stud.acs.upb.ro>
2023-01-10 23:19:59 +0200

    homework-2: first 10p

--- homework/2/cool/scopes/GlobalScope.java
+++ homework/2/cool/scopes/GlobalScope.java
@@ -4,45 +4,30 @@ import java.util.*;
 
 import cool.symbols.Symbol;
 
-public class DefaultScope implements Scope {
-    
-    private Map<String, Symbol> symbols = new LinkedHashMap<>();
-    
-    private Scope parent;
-    
-    public DefaultScope(Scope parent) {
-        this.parent = parent;
-    }
+public class GlobalScope implements Scope {
+    Map<String, Symbol> symbols = new LinkedHashMap<>();
 
     @Override
     public boolean add(Symbol sym) {
         // Reject duplicates in the same scope.
         if (symbols.containsKey(sym.getName()))
             return false;
-        
+
         symbols.put(sym.getName(), sym);
-        
+
         return true;
     }
 
     @Override
     public Symbol lookup(String name) {
-        var sym = symbols.get(name);
-        
-        if (sym != null)
-            return sym;
-        
-        if (parent != null)
-            return parent.lookup(name);
-        
-        return null;
+        return symbols.get(name);
     }
 
     @Override
     public Scope getParent() {
-        return parent;
+        return null;
     }
-    
+
     @Override
     public String toString() {
         return symbols.values().toString();
